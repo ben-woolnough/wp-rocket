4d40e0a38b820ddaa134bc9c6d3a9faaae372adc
Ben Woolnough <ben.woolnough76@hotmail.com>
2017-07-16 15:29:27 +0100

    header formatted correctly

--- header.php
+++ header.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title><?php bloginfo('name'); ?></title>
-  <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
-  <style>
-    body {
-      background: <?php echo get_theme_mod('background_colour', '#fff'); ?>;
-    }
-  </style>
-  <?php wp_head(); ?>
-</head>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title>
+      <?php bloginfo('name'); ?> | 
+      <?php is_front_page() ? bloginfo('description') : wp_title(''); ?>
+    </title>
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+    <style>
+      body {
+        background: <?php echo get_theme_mod('background_colour', '#fff'); ?>;
+      }
+    </style>
+    <?php wp_head(); ?>
+  </head>
 <body>
-
-<!-- <div class="logo">
-  <h1><?php bloginfo('name'); ?></h1>
-</div> -->
 
 <?php if (get_theme_mod('logo_image', '') == '') : ?>
   <h1 id="logo-text"><?php bloginfo('name'); ?></h1>
