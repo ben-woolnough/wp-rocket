47c7c5219d70b643c587103d723143e27420fd98
Ben Woolnough <ben.woolnough76@hotmail.com>
2017-07-16 17:47:54 +0100

    sidebar implemented

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
+<?php if (is_home() || is_archive()) : ?>
+  <?php get_sidebar(); ?>
+<?php endif; ?>
+
 <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('content', get_post_format()); ?>
---
+++ two-column-page.php
@@ -0,0 +1,7 @@
+<?php /* Template Name: Two column page */ ?>
+
+<?php get_header(); ?>
+
+hi
+
+<?php get_footer(); ?>
